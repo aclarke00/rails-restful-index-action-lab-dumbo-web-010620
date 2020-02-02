b43ca516edb40674585fb95b95e34f27e9194358
Alyssa Clarke <alyssa.clarke00@gmail.com>
2020-02-02 15:47:46 -0500

    Done.

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,11 @@
+class StudentsController < ApplicationController
+
+    def index
+        @students = Student.all
+    end
+
+    def show
+        @students = Student.all.find(params[:id])
+    end
+
+end
